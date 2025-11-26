6e08ecdbff984061e73c223cc836f0c0e3a4f4b7
Mateusz <151372273+MatthewCDV@users.noreply.github.com>
2025-11-26 12:29:47 +0100

    menu

--- Assets/Scripts/BasicSpawner.cs
+++ Assets/Scripts/BasicSpawner.cs
@@ -230,18 +230,33 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
         });
     }
 
-    private void OnGUI()
+    //private void OnGUI()
+    //{
+    //    if (_runner == null)
+    //    {
+    //        if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+    //        {
+    //            StartGame(GameMode.Host);
+    //        }
+    //        if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
+    //        {
+    //            StartGame(GameMode.Client);
+    //        }
+    //    }
+    //}
+
+    public void StartGameHost()
     {
         if (_runner == null)
         {
-            if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
-            {
-                StartGame(GameMode.Host);
-            }
-            if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
-            {
-                StartGame(GameMode.Client);
-            }
+            StartGame(GameMode.Host);
+        }
+    }
+    public void StartGameClient()
+    {
+        if (_runner == null)
+        {
+            StartGame(GameMode.Client);
         }
     }
 }
